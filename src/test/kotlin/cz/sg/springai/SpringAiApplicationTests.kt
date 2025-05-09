d7cb9dc9a115546827965c2c898b68ff154c16d6
vondracek <luvo@ciklum.com>
2025-05-09 10:27:15 +0200

    init project

---
+++ src/test/kotlin/cz/sg/springai/SpringAiApplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.sg.springai
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
